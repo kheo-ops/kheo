56bfbf64aea87d011a429091102c399a3c1a90eb
Mikael Gibert <mikael.gibert@gmail.com>
2014-12-29 21:07:54 +0100

    Add basic CRUD support

--- src/main/java/com/migibert/kheo/resources/ServerResource.java
+++ src/main/java/com/migibert/kheo/resources/ServerResource.java
@@ -3,23 +3,62 @@ package com.migibert.kheo.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.codahale.metrics.annotation.Timed;
-import com.google.common.collect.Lists;
-import com.migibert.kheo.core.NetworkInterface;
 import com.migibert.kheo.core.Server;
 
 @Path("servers")
 @Produces(MediaType.APPLICATION_JSON)
 public class ServerResource {
+	
+	private List<Server> serverStore = new ArrayList<>();
 
 	@GET
 	@Timed
 	public List<Server> getServers() {
-		return Lists.newArrayList(new Server("localhost", 4096, 2, new ArrayList<NetworkInterface>()));
+		return serverStore;
 	}
+	
+	@GET
+	@Timed
+	@Path("/{hostname}")
+	public Server getServer(@PathParam("hostname") String hostname) {
+		for(Server server : serverStore) {
+			if(hostname.equals(server.getHostname())) {
+				return server;
+			}
+		}
+		return null;
+	}
+	
+	@POST
+	@Timed
+	public void createServer(Server server) {
+		serverStore.add(server);
+	}
+	
+	@PUT
+	@Timed
+	@Path("/{hostname}")
+	public void updateServer(@PathParam("hostname") String hostname, Server server) {
+		serverStore.remove(getServer(hostname));
+		serverStore.add(server);
+	}
+	
+	@DELETE
+	@Timed
+	@Path("/{hostname}")
+	public void deleteServer(@PathParam("hostname") String hostname, Server server) {
+		serverStore.remove(getServer(hostname));
+	}
+	
+	
 }
