26de887db4fe869884aa68c63958e4cec2defd7f
Mikael Gibert <mikael.gibert@gmail.com>
2015-01-13 21:07:09 +0100

    Adding log

--- kheo-api/src/main/java/com/migibert/kheo/jobs/ConfigurationDiscoveryJob.java
+++ kheo-api/src/main/java/com/migibert/kheo/jobs/ConfigurationDiscoveryJob.java
@@ -13,27 +13,34 @@ import javax.ws.rs.core.Response.Status;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.migibert.kheo.core.Server;
 
 public class ConfigurationDiscoveryJob implements Job {
 
+	private Logger logger = LoggerFactory.getLogger(ConfigurationDiscoveryJob.class);
 	private Client client = ClientBuilder.newClient();
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
+		logger.info("Starting discovery job!");
 		Response getServersResponse = client.target("http://localhost:8080").path("servers").request().accept(MediaType.APPLICATION_JSON).get();
-		if(getServersResponse.getStatus() != Status.OK.getStatusCode()) {
+		if (getServersResponse.getStatus() != Status.OK.getStatusCode()) {
 			return;
 		}
-		
-		Set<Server> servers = getServersResponse.readEntity(new GenericType<Set<Server>>(){});
-		for(Server server : servers) {
+
+		Set<Server> servers = getServersResponse.readEntity(new GenericType<Set<Server>>() {
+		});
+		for (Server server : servers) {
 			Response discoveryResponse = client.target("http://localhost:8080").path("servers/" + server.hostname + "/discover").request().get();
-			if(discoveryResponse.getStatus() == Status.OK.getStatusCode()) {
+			if (discoveryResponse.getStatus() == Status.OK.getStatusCode()) {
 				Server discoveredServer = discoveryResponse.readEntity(Server.class);
-				client.target("http://localhost:8080").path("servers/" + server.hostname).request().put(Entity.entity(discoveredServer, MediaType.APPLICATION_JSON));
+				client.target("http://localhost:8080").path("servers/" + server.hostname).request()
+						.put(Entity.entity(discoveredServer, MediaType.APPLICATION_JSON));
 			}
 		}
+		logger.info("Ending discovery job");
 	}
 }
