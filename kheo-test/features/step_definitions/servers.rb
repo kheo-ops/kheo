0763d700103af9020487b92d0516b53afa6836eb
Mikael Gibert <mikael.gibert@numergy.com>
2015-01-27 18:42:14 +0100

    Implementing server feature tests

--- kheo-test/features/step_definitions/servers.rb
+++ kheo-test/features/step_definitions/servers.rb
@@ -4,21 +4,28 @@ require 'test/unit/assertions'
 World(Test::Unit::Assertions)
 
 Given(/^A server "(.*?)" with access enabled to "(.*?)" with "(.*?)"$/) do |host, user, password|
-    @response = HTTParty.post('http://localhost:8080/servers',
+    @server = {
+        "host" => host,
+        "user" => user,
+        "password" => password
+    }
+end
+
+When(/^I add a "(.*?)" server with access enabled to "(.*?)" with "(.*?)"$/) do |host, user, password|
+    HTTParty.post('http://localhost:8080/servers',
         {
-            :body => { "host" => host, "user" => user, "password" => password }.to_json,
+            :body => @server.to_json,
             :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
         })
 end
 
-When(/^I add a "(.*?)" server with access enabled to "(.*?)" with "(.*?)"$/) do |arg1, arg2, arg3|
-  pending # express the regexp above with the code you wish you had
+Then(/^I can retrieve the server "(.*?)"$/) do |host|
+    @response = HTTParty.get('http://localhost:8080/servers/' + host,
+        {
+            :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+        })
 end
 
-Then(/^I can retrieve the server "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^SSH connectivity is "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^SSH connectivity is "(.*?)"$/) do |connectivity|
+    assert_equal(JSON.parse(@response.body)['sshConnectionValidity'], connectivity)        
 end
