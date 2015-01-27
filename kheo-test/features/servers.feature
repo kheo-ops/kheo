41e89138645e74f812c85836448637e3a4aefb70
Mikael Gibert <mikael.gibert@numergy.com>
2015-01-27 19:27:45 +0100

    Implementing DELETE tests

--- kheo-test/features/servers.feature
+++ kheo-test/features/servers.feature
@@ -6,3 +6,13 @@ Feature: Manage servers
         Then I can retrieve the server "localhost"
         And SSH connectivity is "true"
 
+    Scenario: As a user, I can remove an existing server
+        Given An existing server "server_to_remove"
+        When I remove "server_to_remove"
+        And I retrieve the server "server_to_remove"
+        Then "server_to_remove" is not found
+
+    Scenario: As a user, I cannot remove a non existing server
+        Given A non existing server "server_to_remove"
+        When I remove "server_to_remove"
+        Then "server_to_remove" is not found
--- kheo-test/features/step_definitions/servers.rb
+++ kheo-test/features/step_definitions/servers.rb
@@ -29,3 +29,35 @@ end
 Then(/^SSH connectivity is "(.*?)"$/) do |expectedSshConnectivity|
     assert_equal(expectedSshConnectivity, JSON.parse(@response.body)['sshConnectionValidity'].to_s)
 end
+
+Given(/^An existing server "(.*?)"$/) do |host|
+    HTTParty.post('http://localhost:8080/servers',
+        {
+            :body => { "host" => host, "user" => "user", "password" => "password" }.to_json,
+            :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+        })
+end
+
+When(/^I remove "(.*?)"$/) do |host|
+    @response = HTTParty.delete('http://localhost:8080/servers/' + host)
+end
+
+When(/^I retrieve the server "(.*?)"$/) do |host|
+    @response = HTTParty.get('http://localhost:8080/servers/' + host,
+        {
+            :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+        })
+end
+
+Given(/^A non existing server "(.*?)"$/) do |host|
+    @response = HTTParty.get('http://localhost:8080/servers/' + host,
+        {
+            :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+        })
+    assert_equal(404, @response.code)
+end
+
+Then(/^"(.*?)" is not found$/) do |host|
+    assert_equal(404, @response.code)
+end
+
