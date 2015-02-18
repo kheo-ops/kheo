38378d4f44c077c3214c48759fc2ec058bbe0128
Mikael Gibert <mikael.gibert@gmail.com>
2015-02-18 01:21:33 +0100

    Use plugins to generate tabs

--- kheo-web/app/scripts/controllers/serverDetail.js
+++ kheo-web/app/scripts/controllers/serverDetail.js
@@ -2,5 +2,25 @@
 var module = angular.module('kheoApp');
 
 module.controller('ServerDetailCtrl', ['$scope', '$resource', '$routeParams', 'configuration', function ($scope, $resource, $routeParams, configuration) {
-    $scope.server = $resource(configuration.backend + '/servers/' + $routeParams.hostname).get();   
+    $scope.plugins = $resource(configuration.backend + '/plugins').query();
+    $scope.server = $resource(configuration.backend + '/servers/' + $routeParams.hostname).get();
+    $scope.pluginProperties = [];
+
+    $scope.filterPluginProperties = function(plugin) {    	
+    	$scope.pluginProperties = _.filter($scope.server.serverProperties, function(property) {
+          return property.type == plugin.propertyName;
+    	});    	    	
+    }
+
+    $scope.extractKeyValue = function(property) {
+    	var res = [];
+    	for(var key in property) {            
+            var keyValue = { 
+              key: key,
+              value: property[key]
+            };
+            res.push(keyValue);
+    	}
+    	return res;
+    }
 }]);
